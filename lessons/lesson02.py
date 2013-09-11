af4277310fa4ed1a632cd0c3cdffba1ded12a3ad
trai25 <trai25@gmail.com>
2013-09-11 10:00:24 +0800

    添加笔记 02,04

---
+++ lessons/lesson02.py
@@ -0,0 +1,3 @@
+﻿#Crossin lesson02 print
+ 
+print "Hello World!"  #在 Python 中，使用 print 命令输出内容，需要给要输出的内容加上""或''。
---
+++ lessons/lesson04.py
@@ -0,0 +1,9 @@
+﻿#Crossin lesson04 输入
+ 
+#python有一个接收命令行下输入的方法：input()
+ 
+print "Who do you think I am?"
+input() #和print不同，input必须加上()
+print "Oh, yes!"
+ 
+#运行该文件，会直接显示「Who do you think I am?」这句话，接下来输入答案（如果答案是字符串，需要加上''或""，如果是数字或变量则不用。），程序会回答「Oh, yes!」
